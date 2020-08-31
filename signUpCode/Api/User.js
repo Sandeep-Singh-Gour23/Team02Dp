b4fbf414d692e7b0077276abed7914812e899f22
Sandeep Singh <67502862+Sandeep-Singh-Gour23@users.noreply.github.com>
2020-08-31 18:23:38 +0530

    add bcryptjs

--- signUpCode/Api/User.js
+++ signUpCode/Api/User.js
@@ -3,18 +3,24 @@ const mongoose = require('mongoose');
 const User = require('../DB/User');
 //const route = express.route();
 const route = require('express').Router();
+const { genSaltSync, hashSync } = require('bcrypt');
 
 route.post('/',async(req,res)=>{
-      const{firstName,lastName,emailId,password} = req.body;
+      const body = req.body;
       try{
+      const salt = genSaltSync(10);
+      body.password = hashSync(body.password, salt);
       let demo = {};
-      demo.firstName = firstName;
-      demo.lastName = lastName;
-      demo.emailId = emailId;
-      demo.password = password;
+      demo.firstName = body.firstName;
+      demo.lastName = body.lastName;
+      demo.emailId = body.emailId;
+      demo.password = body.password;
       let userModel = new User(demo);
       await userModel.save();
-      res.json(userModel);
+      res.json({
+       success: 1,
+        message: "you have registered successfully .."
+      });
       }catch (err) {
         res.status(500).send(err);
       }
